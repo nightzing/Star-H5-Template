0542703e4a0191ca37c9991f7a1b1fee361feb32
xiaobo <xiaobo@duc.cn>
2017-01-19 15:15:15 +0800

    version 1.0

---
+++ js/H5ComponentBase.js
@@ -0,0 +1,34 @@
+/* 基本图文组件对象 */
+var H5ComponentBase = function( name,cfg ){
+        var cfg = cfg || {};
+        var id = ( 'h5_c_'+Math.random() ).replace('.','_'); //任意分配id值
+
+        //把当前的组建类型添加到样式中进行标记
+        var cls = ' h5_component_'+cfg.type  ;
+        var component = $('<div class="h5_component'+cls+' h5_component_name_'+name+'"  id="'+id+'">');
+
+        cfg.text && component.text(cfg.text);
+        cfg.width && component.width(cfg.width/2);
+        cfg.height && component.height(cfg.height/2);
+       
+        cfg.css && component.css( cfg.css );
+        cfg.bg && component.css('backgroundImage','url('+cfg.bg+')');
+        if (cfg.center === true) {
+                component.css({
+                        marginLeft : ( cfg.width/4 * -1) + 'px',
+                        left: '50%',
+                })
+        }
+        // ... 很多自定义的参数
+       component.on('onLoad', function() {
+           component.addClass(cls+'_load').removeClass(cls+'_leave');
+           cfg.animateIn && component.animate( cfg.animateIn );
+           return false;
+        });
+       component.on('onLeave', function() {
+           component.addClass(cls+'_leave').removeClass(cls+'_load')
+           cfg.animateOut && component.animate( cfg.animateOut );
+           return false;
+        });
+        return component;
+}
